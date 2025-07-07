0a430a1fbba5ded2d429a3d4a35a8d33d1c71846
Anastasis Dasyras <anastasisdasy@gmail.com>
2025-07-07 20:50:43 +0100

    get all puzzles in home page

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -5,19 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Puzzle;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         // If user is not logged in, redirect to login
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
+        // Get all puzzles
+        $puzzles = $entityManager->getRepository(Puzzle::class)->findAll();
+
         return $this->render('home/index.html.twig', [
             'user' => $this->getUser(),
+            'puzzles' => $puzzles,
         ]);
     }
 }
