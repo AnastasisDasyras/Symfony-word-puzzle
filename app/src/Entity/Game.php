8d6181adfe87b7de56cbbfa8bf7b04f8fba48ced
Anastasis Dasyras <anastasisdasy@gmail.com>
2025-07-07 20:22:16 +0100

    create game model

---
+++ app/src/Entity/Game.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+#[ORM\Table(name: 'game')]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $userId = null;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $puzzleId = null;
+
+    #[ORM\Column(type: 'integer')]
+    private ?int $score = null;
+
+
+    #[ORM\Column(type: 'datetime')]
+    private ?\DateTimeInterface $createdAt = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getPuzzleId(): ?int
+    {
+        return $this->puzzleId;
+    }
+
+    public function setPuzzleId(int $puzzleId): static
+    {
+        $this->puzzleId = $puzzleId;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): static
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
